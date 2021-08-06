56934b5ca7d84cbb12113befbaece0b728c41faa
Bishal Bagale <74647462+bishalbagale@users.noreply.github.com>
2021-08-06 23:01:48 +0545

    final

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import colorama
 from requests_html import HTMLSession
 from colorama import Fore
 import subprocess
@@ -5,13 +6,19 @@ import sys
 import time
 
 session = HTMLSession()
-
-pageNo = 1
+# colorama.init()
+red = Fore.RED
+green = Fore.GREEN
+lightBlue = Fore.LIGHTBLUE_EX
+lightCyan = Fore.LIGHTCYAN_EX
+yellow = Fore.YELLOW
+white = Fore.WHITE
+magenta = Fore.MAGENTA
 
 
 def banner():
-    print("""\
-        
+    print(f"""\{magenta}
+
 ███╗░░░███╗░█████╗░██╗░░░██╗██╗███████╗░░░░░░░█████╗░██╗░░░░░██╗
 ████╗░████║██╔══██╗██║░░░██║██║██╔════╝░░░░░░██╔══██╗██║░░░░░██║
 ██╔████╔██║██║░░██║╚██╗░██╔╝██║█████╗░░█████╗██║░░╚═╝██║░░░░░██║
@@ -20,147 +27,142 @@ def banner():
 ╚═╝░░░░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝╚══════╝░░░░░░░╚════╝░╚══════╝╚═╝""")
     print()
     print()
-    print("\t\t\t\t\t created by: bishalbagale")
+    print(f"{green}\t\t\t\t\t developed by: bishalbagale{white}")
     print()
 
 
-def SorM():
-    global category
-    global searchInput
-    series_or_movie = input("category? series or movie [s/m]? : ")
-    print(f"{Fore.GREEN}press * and hit enter to go back{Fore.WHITE}")
-    searchInput = input("search : ")
-    if series_or_movie.upper() == "S":
+def main():
+    currentPage = 1
+    category = input(
+        f"Enter your prefered category (series/movie)[s/m]: ")
+    if category.upper() == "S":
         category = "TV"
-        search(category, pageNo, searchInput)
-    elif series_or_movie.upper() == "M":
+    elif category.upper() == "M":
         category = "Movies"
-        search(category, pageNo, searchInput)
     else:
-        print(f"{Fore.RED}choose correct category !{Fore.WHITE}")
-        SorM()
+        print(f"{green}Please select s or m{white}")
+        main()
+
+    searchParam = input("search : ")
+    try:
+        scraper(category, searchParam, currentPage)
+    except:
+        pass
 
 
 def nextpageLink(rawLinks):
     for each in rawLinks:
-        if "/torrent" in each: return each
-    else: pass
-
-
-def search(cat, pgNo, searchInput):  #search
-    count = 0  # initial no. of search results
-
-    if searchInput != "*":  #proceed with search dont go back to choose category
-        search = searchInput.replace(" ", "%20")
-        print()
-        url = f"https://www.1377x.to/category-search/{search}/{cat}/{pgNo}/"
-        r = session.get(url)
-
-        # get the link to the next page from search results
-
-        tbody = r.html.find(
-            "tbody", first=True)  #get the tablebody containing search result
-        searchResults = tbody.find(
-            "tr")  #list of search results //contents of the table
-
-        resultDict = {}  #dictionary of link to next page
-        if len(searchResults) != 0:  #validate the result
-            for result in searchResults:
-                count += 1
-
-                seeds = result.find(".seeds", first=True).text
-                leeches = result.find(".leeches", first=True).text
-                size = result.find(".size", first=True).text
-                # uploader = result.find(".uploader", first=True).text
-                name = result.find(".name", first=True).text
-                nextpage = nextpageLink(result.absolute_links)
-                resultDict[count] = nextpage  #add to the dictionary
-                #alter the color of output
-                if count % 2 == 0: color = Fore.LIGHTBLUE_EX
-                else: color = Fore.LIGHTCYAN_EX
-                print(
-                    f"{color}{count}) {name} -->se:{seeds} -->le:{leeches} -->size:{size}"
-                )
-            try:  #check if multiple page results are available
-
-                page = r.html.find(".pagination", first=True).find(
-                    "ul", first=True).find(
-                        "li"
-                    )[-1].text  # grab last li tag to get the total no. of page
-                print()
-                print(
-                    f"\t\t\t\t\t\t\t{Fore.LIGHTYELLOW_EX}page : {pgNo}/{page}{Fore.WHITE}"
-                )
-                print(
-                    f"{Fore.GREEN}\n# press p to change page or press the indexed no. to choose the content and press enter{Fore.WHITE}"
-                )
-                chooseMovie(resultDict)
-
-            except AttributeError:
-                # In case of single page result
-
-                print(f"\t\t\t\t\t\t\t{Fore.LIGHTYELLOW_EX}page : 1/1")
-                chooseMovie(resultDict)
-
-        else:  # no valid result found
-            print(
-                Fore.RED +
-                "No match found! \n consider checking spelling or come back later !"
-            )
+        if "/torrent" in each:
+            return each
     else:
-        SorM()  #go back to choose category
+        pass
 
 
-def moreResults():
-    pg = input("goto page-no. : ")
-    search(category, pg, searchInput)
+def scraper(category, searchParam, currentPage):
+    count = 0  # initial no. of search results
+    searchParam = searchParam.replace(" ", "%20")
+    url = f"https://www.1377x.to/category-search/{searchParam}/{category}/{currentPage}/"
+    r = session.get(url)
+
+    # get the link to the next page from search results
+    tbody = r.html.find(
+        "tbody", first=True)  # get the tablebody containing search result
+    searchResults = tbody.find(
+        "tr")  # list of search results //contents of the table
+    resultDict = {}  # dictionary of link to next page
+    if len(searchResults) != 0:  # validate the result
+
+        for result in searchResults:
+            count += 1
+
+            seeds = result.find(".seeds", first=True).text
+            leeches = result.find(".leeches", first=True).text
+            size = result.find(".size", first=True).text
+            name = result.find(".name", first=True).text
+            nextpage = nextpageLink(result.absolute_links)
+            resultDict[count] = nextpage  # add to the dictionary
+            # alter the color of output
+            if count % 2 == 0:
+                color = lightBlue
+            else:
+                color = lightCyan
+            print(
+                f"{color}{count}) {name} -->se:{seeds} -->le:{leeches} -->size:{size}")
+        try:  # check if multiple page results are available
+
+            page = r.html.find(".pagination", first=True).find(
+                "ul", first=True).find(
+                    "li"
+            )[-1].text  # total no of pages
+            print()
+            print(
+                f"\t\t\t\t\t\t\t{yellow}page : {currentPage}/{page}{white}"
+            )
+            print(
+                f"{green}\n# press p to change page or press the indexed no. to choose the content and press enter{white}"
+            )
+            chooseContent(resultDict, category, searchParam)
+        except AttributeError:
+            # In case of single page result
+
+            print(f"\t\t\t\t\t\t\t{yellow}page : 1/1")
+            chooseContent(resultDict, category, searchParam)
+    else:  # in case of no valid result found
+        print(f"{red}No match found !!\n check your spelling and try again later !")
+        exit()
 
 
-def chooseMovie(resultDict):
+# choose the content from the results page
+def chooseContent(resultDict, category, searchParam):
 
-    indexInput = input()
+    indexInput = input(">> ")
     if indexInput.upper() == "P":
-        moreResults()
+        moreResults(category, searchParam)
     else:
         try:
             mLink = resultDict[int(indexInput)]
             contentChoosed(mLink)
-        except KeyError:
+        except KeyError:  # in case of wrong index no.
 
-            print(f"{Fore.RED}Invalid input !! Enter valid input{Fore.WHITE}")
-            chooseMovie(resultDict)
+            print(f"{red}Invalid input !! Enter valid input{white}")
+            chooseContent(resultDict)  # go back to choosing content
 
 
-# Scrape the torrent link of the content
-def contentChoosed(mLink):
+def moreResults(category, searchParam):  # change page no. / view more results
+    pg = input("goto page-no. : ")
+    scraper(category, searchParam, pg,)
+
+
+def contentChoosed(mLink):  # Scrape the torrent link of the content
     r = session.get(mLink)
     links = r.html.absolute_links
     for link in links:
         if "magnet:?" in link:
             torrentLink = link
-    downloadORstream(torrentLink)
+    play(torrentLink)
 
 
-def streamingMedium():
+def streamingMedium():  # choose streaming medium vlc/mpv
 
-    medium = input("want to stream though vlc or mpv [v/m]? ")
+    medium = input("want to stream through vlc or mpv [v/m]? ")
     if medium.upper() == "V":
-        return "vlc"
+        return "vlc"  # stream through vlc
     elif medium.upper() == "M":
-        return "mpv"
+        return "mpv"  # stream through mpv
     else:
-        print(f"\n {Fore.RED}invalid input !{Fore.WHITE}")
+        print(f"\n {red}invalid input !{white}")
         streamingMedium()
 
 
-def downloadORstream(torrentLink):
+def play(torrentLink):  # play the torrentlink
 
     cmd = []
     cmd.append("webtorrent")
+    # checck download or stream option
     verify_download = input("want to stream or download [s/d]? ")
-    if verify_download.upper() == "D":
+    if verify_download.upper() == "D":  # download
         cmd.append(f'"{torrentLink}"')
-    elif verify_download.upper() == "S":
+    elif verify_download.upper() == "S":  # stream
         medium = streamingMedium()
         cmd.append(f'"{torrentLink}"')
         if medium == "vlc":
@@ -169,17 +171,19 @@ def downloadORstream(torrentLink):
             cmd.append("--mpv")
 
     else:
-        print(f"\n {Fore.RED}invalid input !{Fore.WHITE}")
-        downloadORstream(torrentLink)
+        # In case of wrong option choosed (stream/download)
+        print(f"\n {red}invalid input !{white}")
+        play(torrentLink)
 
-    if sys.platform.startswith('win32'):
+    if sys.platform.startswith('win32'):  # for windows
         subprocess.call(cmd, shell=True)
-    elif sys.platform.startswith('linux'):
+    elif sys.platform.startswith('linux'):  # for linux
         subprocess.call(cmd)
-    elif sys.platform.startswith('darwin'):
-        print("\n tf you doing here rich kid ? pay for your movie !!")
+    elif sys.platform.startswith('darwin'):  # for osx
+        print("\n tf you doing here rich kid ? pay for your movie !!")  # DIY
 
 
+# calling
 banner()
-time.sleep(1)
-SorM()
+time.sleep(.5)
+main()
